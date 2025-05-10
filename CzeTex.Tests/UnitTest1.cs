1cb97b2af564efcd352a950c61814eacfac3fdd9
Jakub Oliver Kubin <jakub.oliver.kubin@gmail.com>
2025-05-10 22:15:55 +0200

    Vytvoreni testu pro existenci souboru

--- CzeTex.Tests/UnitTest1.cs
+++ CzeTex.Tests/UnitTest1.cs
@@ -3,10 +3,18 @@
 using Xunit;
 using CzeTex;
 
+using System;
+
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void CanReadFile()
     {
+        Assert.Equal("Hello, World!", new Files(new string[] {"examples", "example1.txt"}).LoadFile()[0]);
+    }
+
+    [Fact]
+    public void FileDoesNotExist(){
+        Assert.Throws<System.IO.FileNotFoundException>(() => new Files(new string[] {"examples", "neexistuje.txt"}));
     }
 }
